f7c360f3788150b086e35f5346da58b356d86cfe
hudaaeda10 <hudaaeda10@gmail.com>
2020-10-06 10:53:55 +0700

    video

--- resources/views/sites/video.blade.php
+++ resources/views/sites/video.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-<section class="hero-wrap hero-wrap-2" style="background-image: url('{{asset('sites')}}/images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+<section class="hero-wrap hero-wrap-2" style="background-image: url('/sites/images/bg_2.jpg');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text align-items-end">
@@ -18,15 +18,15 @@
         <div class="row d-flex">
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <iframe href="https://www.youtube.com/watch?v=J_i1XQmp0e0" class="block-20 rounded" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <a href="https://www.youtube.com/watch?v=J_i1XQmp0e0" class="block-20 rounded popup-vimeo d-flex justify-content-center align-items-center" style="background-image: url('');">
+                        <span class="fa fa-play fa-3x"></span>
                     </a>
                     <div class="text mt-3">
                         <div class="meta mb-2">
                             <div><a href="#">January 30, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
+                            <div><a href="#" class="meta-chat">categories</a></div>
                         </div>
-                        <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                        <h3 class="heading"><a href="https://www.youtube.com/watch?v=J_i1XQmp0e0">Even the all-powerful Pointing has no control about the blind texts</a></h3>
                     </div>
                 </div>
             </div>
@@ -47,7 +47,7 @@
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('{{asset('sites')}}/images/image_3.jpg');">
+                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('/sites/images/image_3.jpg');">
                     </a>
                     <div class="text mt-3">
                         <div class="meta mb-2">
@@ -61,7 +61,7 @@
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('{{asset('sites')}}/images/image_4.jpg');">
+                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('/sites/images/image_4.jpg');">
                     </a>
                     <div class="text mt-3">
                         <div class="meta mb-2">
@@ -75,7 +75,7 @@
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('{{asset('sites')}}/images/image_5.jpg');">
+                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('/sites/images/image_5.jpg');">
                     </a>
                     <div class="text mt-3">
                         <div class="meta mb-2">
@@ -89,7 +89,7 @@
             </div>
             <div class="col-md-4 d-flex ftco-animate">
                 <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('{{asset('sites')}}/images/image_6.jpg');">
+                    <a href="blog-single.html" class="block-20 rounded" style="background-image: url('/sites/images/image_6.jpg');">
                     </a>
                     <div class="text mt-3">
                         <div class="meta mb-2">
